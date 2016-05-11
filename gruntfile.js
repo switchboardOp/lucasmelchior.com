0f7ca2893cf2056f896bda5e8ffd6f379cce8cd2
Lucas Melchior <lm@link.domain>
2016-05-11 03:00:10 -0500

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+        , sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'style.sass'
+                }
+            }
+        }
+        , cssmin: {
+            options: {
+                shorthandCompacting: false
+                , roundingPrecision: -1
+            }
+            , target: {
+                files: {
+                    'css/style.min.css': 'css/style.css'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.sass'
+                , tasks: ['sass', 'cssmin']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('default', ['watch']);
+}
